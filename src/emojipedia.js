e5a9f1ae2305f132e4d419c9739f3cae0065f2fb
Erin C <101282477+eireann07@users.noreply.github.com>
2022-07-10 11:30:40 -0500

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🧝🏻‍♀️",
+    name: "Aelin",
+    description:
+      "Aelin Ashryver Whitethorn Galathynius, Queen of Terrasen. Also Aelin of the Wildfire, Heir of Fire. Formerly known as Celaena Sardothien, Adarlan's Assasin. Mate to Rowan Whitethorn."
+  },
+  {
+    id: 2,
+    emoji: "🧝🏻",
+    name: "Rowan",
+    description:
+      "Rowan Whitethorn, Prince Consort of Terrasen. Formerly of Doranelle, and of Maeve's cabal of warriors. Mate to Aelin Galathynius."
+  },
+  {
+    id: 3,
+    emoji: "🤴🏻",
+    name: "Dorian",
+    description:
+      "Dorian Havilliard, King of Adarlan. Possessed by a Valg prince before freeing himself while in combat with Aelin Galathynius. Current lover of Manon Blackbeak, Witch Queen."
+  },
+  {
+    id: 4,
+    emoji: "⚔️",
+    name: "Chaol",
+    description:
+      "Chaol Westfall, former Captain of the Guard of Adarlan, Hand of the King of Adarlan. Paralytically injured by the Valg, but healed by his now-wife, Yrene Towers"
+  },
+  {
+    id: 5,
+    emoji: "👑",
+    name: "Manon",
+    description:
+      "Manon Blackbeak Crochan, Witch Queen. Born half-Ironteeth, half-Crochan witch, raised by the Blackbeak Ironteeth clan. Heir to Rhiannon Crochan. Former commander of the Thirteen coven."
+  },
+  {
+    id: 6,
+    emoji: "🐺",
+    name: "Aedion",
+    description:
+      "Aedion Ashryver, Wolf of the North. Cousin to Aelin, Queen of Terrasen. Formerly of Wendlyn, formerly commander of Adarlan's armies in the North. Husband to Lysandra Ennar, Lady of Caraverre."
+  }
+];
+
+export default emojipedia;
